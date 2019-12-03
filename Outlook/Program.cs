a08b2ed8d22675583b1e61448165419d1842b1b1
isakatirci <ce.isakatirci@gmail.com>
2019-12-03 23:19:15 +0300

    init

---
+++ Outlook/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.Office.Interop.Outlook;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Outlook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application myApp = new Application();
+            NameSpace mapiNameSpace = myApp.GetNamespace("MAPI");
+            MAPIFolder myInbox = mapiNameSpace.GetDefaultFolder(OlDefaultFolders.olFolderInbox);
+            using (StreamWriter sw = new StreamWriter("log.txt"))
+            {
+                foreach (var item in myInbox.Items)
+                {
+                    try
+                    {
+                        if (item is MailItem)
+                        {
+
+                        }
+                        else
+                        {
+                            sw.WriteLine("nameof:", nameof(item));
+                            continue;
+                        }
+                        var item1 = (MailItem)item;
+
+                        if (myInbox.Items.Count > 0)
+                        {
+                            // Grab the Subject
+                            var lblSubject = item1.Subject;
+                            //Grab the Attachment Name
+                            if (item1.Attachments.Count > 0)
+                            {
+                                var lblAttachmentName = item1.Attachments[1].FileName;
+                                foreach (Attachment attachment in item1.Attachments)
+                                {
+                                    try
+                                    {
+                                        sw.WriteLine(attachment.FileName);
+                                        attachment.SaveAsFile("./Dosya/" + attachment.FileName);
+                                    }
+                                    catch (System.Exception ex)
+                                    {
+                                        sw.WriteLine(JsonConvert.SerializeObject(ex));
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                var lblAttachmentName = "No Attachment";
+                            }
+                            // Grab the Body
+                            var txtBody = item1.Body;
+                            // Sender Name
+                            var lblSenderName = item1.SenderName;
+                            // Sender Email
+                            var lblSenderEmail = item1.SenderEmailAddress;
+                            // Creation date
+                            var lblCreationdate = item1.CreationTime.ToString();
+
+                        }
+                        else
+                        {
+                            sw.WriteLine("There are no emails in your Inbox.");
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+
+                        sw.WriteLine(JsonConvert.SerializeObject(ex));
+                    }
+
+                }
+            }
+        }
+    }
+}
--- tr.pdfdrive.com.bot/Program.cs
+++ tr.pdfdrive.com.bot/Program.cs
@@ -1,15 +1,75 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace tr.pdfdrive.com.bot
 {
-    class Program
+    public class MyClass
     {
+
+    }
+    internal class Program
+    {
+        private static HtmlDocument GetHtmlDocument(string url, string fileName)
+        {
+            var web = new HtmlWeb();
+            if (!File.Exists(fileName))
+            {
+                var tempDocument = web.Load(url);
+                tempDocument.Save(fileName, Encoding.UTF8);
+            }
+            var document = new HtmlDocument();
+            document.Load(fileName);
+            return document;
+        }
         static void Main(string[] args)
         {
+            var query = "orhan";
+            var fileName = query + ".txt";
+            var rootLink = "https://tr.pdfdrive.com";
+            var url = rootLink + "/search?q={0}";
+            var link = string.Format(url, query);
+            var document = GetHtmlDocument(link, fileName);
+            
+
+            var list = document.DocumentNode.SelectNodes("//div[@class='files-new']//li");
+            foreach (var item in list.Take(1))
+            {
+                var kitapLink = item.SelectSingleNode(".//div[@class='file-left']//a").Attributes["href"].Value;
+                Console.WriteLine(kitapLink);
+                Console.WriteLine(item.SelectSingleNode(".//div[@class='file-left']//img").Attributes["src"].Value);
+                Console.WriteLine(item.SelectSingleNode(".//div[@class='file-right']//a").InnerText.Trim());
+                Console.WriteLine(item.SelectSingleNode(".//div[@class='file-info']").InnerText.Trim());
+                var temp1 = GetHtmlDocument(rootLink + kitapLink, kitapLink.Replace("/", ""));
+                Console.WriteLine(temp1.DocumentNode.SelectSingleNode("//a[@id='download-button-link']").Attributes["href"].Value);
+                var temp2 = GetHtmlDocument(rootLink + kitapLink, kitapLink.Replace("/", ""));
+                var temp3 = temp2.DocumentNode.SelectSingleNode("//a[@id='download-button-link']").Attributes["href"].Value;
+                Console.WriteLine(temp3);
+                IWebDriver webDriver = new ChromeDriver();
+                WebDriverWait wait = new WebDriverWait(webDriver,TimeSpan.FromSeconds(40));
+                webDriver.Navigate().GoToUrl(rootLink + temp3);
+                wait.Until<bool>(d => {
+                    try
+                    {
+                        Console.WriteLine(webDriver.PageSource);
+
+                        return d.FindElement(By.XPath("//a[@class='btn btn-success btn-responsive']")) == null;
+                    }
+                    catch (Exception)
+                    {
+
+                        return true;
+                    }                                       
+                });
+            }
         }
     }
 }
